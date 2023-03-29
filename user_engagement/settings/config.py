822d4bcf0c10f1c120cb34e174c749e49e6fe58e
Kirk Zmijewski <kirkzmijewski@gmail.com>
2023-03-28 21:13:43 -0400

    restructure files

--- user_engagement/settings/config.py
+++ user_engagement/settings/config.py
@@ -1,4 +1,5 @@
 import os 
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,4 +10,14 @@ class PGConnector():
     DB_USER = os.getenv('DB_USER')
     DB_DATABASE = os.getenv('DB_DATABASE')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+    def connect(self):
+        return psycopg2.connect(
+            database=self.DB_DATABASE,
+            host=self.DB_HOST,
+            user=self.DB_USER,
+            password=self.DB_PASSWORD,
+            port=self.DB_PORT
+        )
+
 
