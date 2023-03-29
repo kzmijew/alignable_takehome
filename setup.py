f421be1e522790b0e106c863846874f5eb7faafe
Kirk Zmijewski <kirkzmijewski@gmail.com>
2023-03-28 21:26:38 -0400

    rename dir

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name='alignable',
+    name='emailtools',
     version='0.0.1',
     description='Mailer tools for email campaigns.',
     author='kirkzmijewski@gmail.com',
